8e872002568b6ff17378748f4ae4f55945c7d198
Albert Wang <albertyw@mit.edu>
2017-02-11 23:11:07 -0800

    Include css file into page

--- gentle-alerts/bootstrap.js
+++ gentle-alerts/bootstrap.js
@@ -1,3 +1,8 @@
 var s = document.createElement("script");
 s.src = chrome.extension.getURL("script.js");
 (document.head||document.documentElement).appendChild(s);
+
+var c = document.createElement("link");
+c.rel = "stylesheet";
+c.href = chrome.extension.getURL("gentle-alerts.css");
+(document.head||document.documentElement).appendChild(c);
