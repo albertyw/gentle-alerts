367340e2de38a7151ba9d97843b68783d743e27f
Albert Wang <albertyw@mit.edu>
2017-02-12 18:03:10 -0800

    Refactor

--- gentle-alerts/bootstrap.js
+++ gentle-alerts/bootstrap.js
@@ -1,7 +1,9 @@
+// Install Javascript
 var s = document.createElement("script");
 s.src = chrome.extension.getURL("script.js");
 (document.head||document.documentElement).appendChild(s);
 
+// Install CSS
 var c = document.createElement("link");
 c.rel = "stylesheet";
 c.href = chrome.extension.getURL("gentle-alerts.css");
--- gentle-alerts/script.js
+++ gentle-alerts/script.js
@@ -1,40 +1,50 @@
-var originalOnclick = window.onclick;
 var modalHTML = '\
-<!-- The Modal -->\
 <div class="gentle-alerts-modal">\
-\
-  <!-- Modal content -->\
   <div class="gentle-alerts-modal-content">\
     <span class="close">&times;</span>\
     <p>Some text in the Modal..</p>\
   </div>\
-\
 </div>';
 
-function getModal() {
-    var modal = document.getElementsByClassName("gentle-alerts-modal");
-    return modal[0];
-}
-
-function generateModal(msg) {
+// Create a modal from modalHTML and append to the bottom of the document
+function createModal(msg) {
     var span = document.createElement("span");
     span.innerHTML = modalHTML;
     document.documentElement.appendChild(span);
     var modal = getModal();
     var modalContent = modal.getElementsByTagName("p")[0];
-    console.log(modalContent);
     modalContent.textContent = msg;
-    modal.style.display = "block";
-    registerModalClose();
-    return span;
+    return modal;
 }
 
+// Find and delete the modal
+function deleteModal() {
+    var modal = getModal();
+    modal.parentNode.removeChild(modal);
+}
+
+// Get the modal DOM element
+function getModal() {
+    var modal = document.getElementsByClassName("gentle-alerts-modal");
+    return modal[0];
+}
+
+// Create, set the modal content, and show it
+function generateModal(msg) {
+    var modal = createModal(msg);
+    modal.style.display = "block";
+    registerModalClose();
+}
+
+// Set up an event to process closing the modal
 function registerModalClose() {
     // When the user clicks anywhere outside of the modal, close it
+    var originalOnclick = window.onclick;
     window.onclick = function gentleAlertsOnclick(onclickEvent) {
         var modal = getModal();
         if (onclickEvent.target == modal) {
-            modal.style.display = "none";
+            deleteModal();
+            window.onclick = originalOnclick;
         }
         if (originalOnclick) {
             originalOnclick(onclickEvent);
@@ -42,9 +52,4 @@ function registerModalClose() {
     };
 }
 
-function modalAlert(msg) {
-    generateModal(msg);
-}
-
-
-window.alert = modalAlert;
+window.alert = generateModal;
