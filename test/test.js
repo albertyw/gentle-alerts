459274a048604c34143a84327cf9e5599254dcb8
Albert Wang <albertyw@mit.edu>
2017-02-24 01:38:57 -0800

    Add behavior tests for modal

--- test/test.js
+++ test/test.js
@@ -17,3 +17,23 @@ describe("Modal", function() {
         expect(modal.modalElement).to.be.undefined;
     });
 });
+
+describe("alert", function() {
+    beforeEach(function() {
+        modal = undefined;
+        $("#gentle-alerts-modal").remove();
+    });
+    it("can show modal", function() {
+        alert("alert text");
+        expect($("#gentle-alerts-modal-content-text").length).to.equal(1);
+        expect($("#gentle-alerts-modal-content-text").text()).to.equal("alert text");
+    });
+    it("can hide the modal", function(done) {
+        alert("alert text");
+        $.when($("#gentle-alerts-modal").trigger("click")).done(function(){
+            expect($("#gentle-alerts-modal-content-text").length).to.equal(0);
+            expect($("#gentle-alerts-modal").length).to.equal(0);
+            done();
+        });
+    });
+});
