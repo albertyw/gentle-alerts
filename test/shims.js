e745e5e7ff32e54bdd1b087c93f58fdd80d197e0
Albert Wang <albertyw@mit.edu>
2017-03-06 00:11:47 -0800

    Add shims to make mocha-phantomjs support new javascript APIs

---
+++ test/shims.js
@@ -0,0 +1,17 @@
+// These are some shims that make mocha-phantomjs support some features in
+// HTML5
+
+// Set `document.currentScript`
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/currentScript
+document.currentScript = {
+    dataset: {
+        notificationAudio: "../gentle-alerts/notification.ogg"
+    }
+};
+
+// Set `HTMLAudioElement`
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement
+var Audio = function Audio(audio) {
+};
+Audio.prototype.play = function play() {
+};
