4d243e3db69076621b4981d930630f1aafcb30b1
Albert Wang <albertyw@mit.edu>
2017-03-05 23:01:53 -0800

    Switch from chrome.extension to chrome.runtime

--- gentle-alerts/bootstrap.js
+++ gentle-alerts/bootstrap.js
@@ -1,10 +1,10 @@
 // Install Javascript
 var s = document.createElement("script");
-s.src = chrome.extension.getURL("script.js");
+s.src = chrome.runtime.getURL("script.js");
 (document.head||document.documentElement).appendChild(s);
 
 // Install CSS
 var c = document.createElement("link");
 c.rel = "stylesheet";
-c.href = chrome.extension.getURL("gentle-alerts.css");
+c.href = chrome.runtime.getURL("gentle-alerts.css");
 (document.head||document.documentElement).appendChild(c);
